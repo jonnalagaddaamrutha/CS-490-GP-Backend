3cae209ee3c33d13eb7b9302881b66305841f429
SandeebAdhikari <sa2734@njit.edu>
2025-10-24 18:51:44 -0400

    Sandeeb: add credentials in .env file and made database.js to work my flow

--- config/database.js
+++ config/database.js
@@ -1,19 +1,13 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const pool = mysql.createPool({
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD, 
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT || 3306,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
-async function query(sql, params) {
-  const [rows] = await pool.execute(sql, params);
-  return rows;
-}
-
-module.exports = { query };
+export default pool;
